07ff68e0a5582d0838d76464cb3f6b23cf5af95c
David Alejandro Serrada Botía <davidasb.developer@gmail.com>
2020-11-22 01:40:30 -0400

    feat: :sparkles: agregadas validaciones para el body

---
+++ src/middlewares.js
@@ -0,0 +1,51 @@
+const yup = require("yup");
+
+let schemaPost = yup.object().shape({
+    first_name: yup.string().required().min(3).max(50),
+    last_name: yup.string().required().min(3).max(50),
+    dni: yup.number().required().min(4).max(50),
+    birth_date: yup.date().required(),
+    sex: yup.string().required().min(1).max(1).matches(/(F|M)/),
+    country: yup.string().required().min(3).max(50),
+    city: yup.string().required().min(3).max(50),
+})
+
+let schemaPatch = yup.object().shape({
+    first_name: yup.string().min(3).max(50),
+    last_name: yup.string().min(3).max(50),
+    dni: yup.number().min(4).max(50),
+    birth_date: yup.date(),
+    sex: yup.string().min(1).max(1).matches(/(F|M)/),
+    country: yup.string().min(3).max(50),
+    city: yup.string().min(3).max(50),
+})
+
+const validationMiddlewarePost = async (req, res, next) => {
+    try {
+        await schemaPost.validate(req.body)
+        next()
+    } catch (err) {
+        res.status(400).json({
+            created: false,
+            error: "field invalido, verifique e intente nuevamente."
+        })
+    }
+}
+
+const validationMiddlewarePatch = async (req, res, next) => {
+    try {
+        await schemaPatch.validate(req.body)
+        next()
+    } catch (err) {
+        res.status(400).json({
+            created: false,
+            error: "field invalido, verifique e intente nuevamente."
+        })
+    }
+}
+
+
+module.exports = {
+    validationMiddlewarePost,
+    validationMiddlewarePatch
+}
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express")
 const { createBeneficiario, listAllBeneficiarios, getBeneficiarioByDni,
     updateBeneficiarioByDni, deleteBeneficiarioByDni } = require("./controllers")
+const { validationMiddlewarePost, validationMiddlewarePatch } = require("./middlewares")
+
 const router = Router()
 
 router.get('/beneficiarios', listAllBeneficiarios)
-router.post('/beneficiario', createBeneficiario)
+router.post('/beneficiario', validationMiddlewarePost, createBeneficiario)
 router.get('/beneficiario/:dni', getBeneficiarioByDni)
-router.patch('/beneficiario/:dni', updateBeneficiarioByDni)
+router.patch('/beneficiario/:dni', validationMiddlewarePatch, updateBeneficiarioByDni)
 router.delete('/beneficiario/:dni', deleteBeneficiarioByDni)
 
 
